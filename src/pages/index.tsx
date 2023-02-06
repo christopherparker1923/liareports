50e7c7e80b6f3c406bbd60eec686406ed78ee3ba
parkec11 <70435944+parkec11@users.noreply.github.com>
2023-02-06 13:47:09 -0500

    auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { type NextPage } from "next";
 import Link from "next/link";
 import { Button, SimpleGrid, Title } from "@mantine/core";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { api } from "../utils/api";
 
 const Home: NextPage = () => {
   return (
@@ -29,9 +31,34 @@ const Home: NextPage = () => {
           <Button className="w-full bg-rhino text-white hover:bg-gray-600">
             Enter
           </Button>
+          <AuthShowcase />
         </Link>
       </SimpleGrid>
     </main>
+  );
+};
+
+const AuthShowcase: React.FC = () => {
+  const { data: sessionData } = useSession();
+
+  const { data: secretMessage } = api.example.getSecretMessage.useQuery(
+    undefined, // no input
+    { enabled: sessionData?.user !== undefined }
+  );
+
+  return (
+    <div className="flex flex-col items-center justify-center gap-4">
+      <p className="text-center text-2xl text-white">
+        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+        {secretMessage && <span> - {secretMessage}</span>}
+      </p>
+      <button
+        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
+      >
+        {sessionData ? "Sign out" : "Sign in"}
+      </button>
+    </div>
   );
 };
 
