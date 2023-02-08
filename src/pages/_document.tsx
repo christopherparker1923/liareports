903360d4aba03fe027989d4460b8edbd43e2c900
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-02-08 11:55:29 -0500

    random stuff

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import { createGetInitialProps, createStylesServer } from "@mantine/next";
+import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,39 +6,36 @@ import { DarkModeToggle } from "../components/DarkModeToggle";
 import { LoginButton } from "../components/LoginButton";
 import type { GetServerSideProps } from "next";
 import { getBasicServerSideProps } from "../services/getBasicSeverSideProps";
+import { useContext } from "react";
 
 const Home: NextPage = () => {
+  // const  theme = useCon[]text(Theme)
   return (
     <main className="min-w-screen flex min-h-screen items-center justify-center bg-gradient-to-b from-blackSqueeze to-neptune dark:from-blackSqueeze dark:to-rhino">
-      <SimpleGrid
-        cols={1}
-        className="container max-w-lg justify-center rounded-md p-10 text-center"
-      >
-        <Card className="min-w-fit rounded-md bg-gray-100 p-5 shadow-md dark:bg-gray-600">
-          <Card.Section className="m-1">
-            <Image
-              src="/lia_logo.png"
-              width={362}
-              height={208}
-              loading="eager"
-              alt="Lineside Industrial Automation logo"
-              className="m-auto "
-            />
-          </Card.Section>
-          <Card.Section>
-            <Title className="m-4 text-3xl text-black dark:text-gray-300">
-              LIAReports
-            </Title>
-          </Card.Section>
-          <Card.Section>
-            <LoginButton />
-          </Card.Section>
-          <Card.Section>
-            <DarkModeToggle />
-          </Card.Section>
-        </Card>
-        <Link href="/dashboard"></Link>
-      </SimpleGrid>
+      <Card className="m-0 flex min-w-fit flex-col items-center justify-center rounded-md bg-gray-100 p-5 shadow-md dark:bg-gray-600">
+        <Card.Section>
+          <Image
+            src="/lia_logo.png"
+            width={362}
+            height={208}
+            loading="eager"
+            alt="Lineside Industrial Automation logo"
+            className="m-auto "
+          />
+        </Card.Section>
+        <Card.Section>
+          <h1 className="m-4 text-3xl font-bold text-black dark:text-gray-300">
+            LIAReports
+          </h1>
+        </Card.Section>
+        <Card.Section>
+          <LoginButton />
+        </Card.Section>
+        <Card.Section className="float-left w-full">
+          <DarkModeToggle />
+        </Card.Section>
+      </Card>
+      <Link href="/dashboard"></Link>
     </main>
   );
 };
