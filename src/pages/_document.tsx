356df6d80a74b98bf6afca7a11210b72d48e969a
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-02-08 12:07:13 -0500

    i hate styles

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,10 +2,7 @@ import { createGetInitialProps } from "@mantine/next";
 import Document, { Head, Html, Main, NextScript } from "next/document";
 import Script from "next/script";
 
-const getInitialProps = createGetInitialProps();
 export default class _Document extends Document {
-  static getInitialProps = getInitialProps;
-
   render() {
     return (
       <Html>
