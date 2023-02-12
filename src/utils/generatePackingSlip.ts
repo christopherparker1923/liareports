227aa0808b6556a15592cbce769f7e02e6742018
Jacob Stephenson <jacobstephenson97@gmail.com>
2023-02-12 16:00:31 -0500

    fix

--- src/utils/generatePackingSlip.ts
+++ src/utils/generatePackingSlip.ts
@@ -1,19 +1,20 @@
 // Import the pdfmake library
 import pdfMake from "pdfmake/build/pdfmake";
 import pdfFonts from "pdfmake/build/vfs_fonts";
+import { TDocumentDefinitions } from "pdfmake/interfaces";
 import { PackingSlipPart } from "../pages/dashboard/generate/packing-slip";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 // Define the PDF document structure
 export async function generatePackingSlip(
   parts: PackingSlipPart[],
-  customer: string = "",
-  billingAdress: string = "",
-  shippingAdress: string = "",
-  orderDate: string = "",
-  orderNumber: string = "",
-  purchaseOrder: string = "",
-  customerContact: string = ""
+  customer = "",
+  billingAdress = "",
+  shippingAdress = "",
+  orderDate = "",
+  orderNumber = "",
+  purchaseOrder = "",
+  customerContact = ""
 ) {
   const getBase64FromUrl = async (url: string): Promise<string> => {
     const data = await fetch(url);
@@ -40,8 +41,6 @@ export async function generatePackingSlip(
     shipQty: part.quantity || 0,
   }));
 
-  console.log(partsList);
-
   const currentDate = new Date();
   const formattedDate = currentDate.toLocaleDateString();
   const colors = {
@@ -61,6 +60,166 @@ export async function generatePackingSlip(
   // playground requires you to assign document definition to a variable called dd
 
   // playground requires you to assign document definition to a variable called dd
+  const body = [
+    [
+      {
+        text: "ITEM #",
+        fillColor: backgroundBlue,
+        color: "white",
+        border: [false, false, false, false],
+      },
+      {
+        text: "DESCRIPTION",
+        fillColor: backgroundBlue,
+        color: "white",
+        border: [false, false, false, false],
+      },
+      {
+        text: "ORDER QTY",
+        fillColor: backgroundBlue,
+        color: "white",
+        border: [false, false, false, false],
+      },
+      {
+        text: "SHIP QTY",
+        fillColor: backgroundBlue,
+        color: "white",
+        border: [false, false, false, false],
+      },
+    ],
+    ...partsList.map((part) => ([
+      Object.values(part).map((value) => ({
+        text: value
+      })
+      ),
+    ])).flat(),
+  ];
+  const docDef: TDocumentDefinitions = {
+    content: [
+      {
+        columns: [
+          {
+            image: await getBase64FromUrl(
+              "/_next/image?url=%2Flogo_noname.png&w=3840&q=75"
+            ),
+            width: 60,
+            fit: [60, 60],
+            margin: [0, 0, 100, 0],
+          },
+          {
+            width: "*",
+
+            text: [
+              {
+                text: "Lineside Industrial Automation Inc.\n",
+                style: "header",
+              },
+              {
+                text: "PACKING SLIP",
+                style: "coloredText",
+                margin: [0, 100, 0, 0],
+              },
+            ],
+          },
+        ]
+      },
+      {
+        canvas: [
+          {
+            type: "line",
+            x1: 0,
+            y1: 5,
+            x2: 595 - 2 * 40,
+            y2: 5,
+            lineWidth: 2,
+          },
+        ],
+      },
+      {
+        margin: [-5, 10, -5, 0],
+        style: "infoText",
+        table: {
+          widths: ["auto", "*", "auto", "auto"],
+          body: [
+            [
+              { text: "60 Ottawa St S", border: [false, false, false, false] },
+              { text: "", border: [false, false, false, false] },
+              { text: "Date:", border: [false, false, false, false] },
+              { text: formattedDate, border: [true, true, true, true] },
+            ],
+            [
+              {
+                text: "Kitchener, ON, N2G 3R5",
+                border: [false, false, false, false],
+              },
+              { text: "", border: [false, false, false, false] },
+              { text: "Customer ID:", border: [false, false, false, false] },
+              { text: customer, border: [true, true, true, true] },
+            ],
+            [
+              {
+                text: "Phone: 519-590-7769 519-504-7906",
+                border: [false, false, false, false],
+              },
+              { text: "", border: [false, false, false, false] },
+              { text: "", border: [false, false, false, false] },
+              { text: "", border: [false, false, false, false] },
+            ],
+            [
+              {
+                text: "Website: www.linesideautomation.com",
+                border: [false, false, false, false],
+              },
+              { text: "", border: [false, false, false, false] },
+              { text: "", border: [false, false, false, false] },
+              { text: "", border: [false, false, false, false] },
+            ],
+          ],
+        },
+      },
+      {
+        margin: [-5, 10, -5, 0],
+        table: {
+          headerRows: 1,
+          widths: ["auto", "auto", "auto", "auto"],
+          body
+        }
+      }
+    ],
+    styles: {
+      header: {
+        fontSize: 22,
+        bold: true,
+        alignment: "right",
+      },
+      coloredText: {
+        fontSize: 22,
+        color: textBlue,
+        alignment: "right",
+      },
+      infoText: {
+        fontSize: 12,
+      },
+      basicText: {
+        fontSize: 14,
+      },
+      blueBackground: {
+        fontSize: 14,
+        color: "white",
+        background: backgroundBlue,
+      },
+      tableExample: {
+        margin: [0, 5, 0, 15],
+        fontSize: 10,
+      },
+      tableHeader: {
+        bold: true,
+        fontSize: 11,
+        color: "black",
+        background: "#e6e6e6",
+      },
+    },
+  };
 
   const docDefinition = {
     content: [
@@ -261,7 +420,16 @@ export async function generatePackingSlip(
                 border: [false, false, false, false],
               },
             ],
-            partsList,
+            [
+              {
+                text: partsList[0]?.text || "",
+                description: partsList[0]?.description || "",
+                orderQty: partsList[0]?.orderQty || 0,
+                shipQty: partsList[0]?.shipQty || 0,
+                border: [false, false, false, false],
+                color: "white"
+              },
+            ],
           ],
         },
       },
@@ -302,5 +470,5 @@ export async function generatePackingSlip(
   };
 
   // Generate the PDF
-  pdfMake.createPdf(docDefinition).open();
+  pdfMake.createPdf(docDef).open();
 }
