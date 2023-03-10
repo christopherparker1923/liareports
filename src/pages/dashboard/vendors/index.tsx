aec68ce29c16dab1479f759c7d242e968f0dafe2
mjwillgetyou <44821370+mjwillgetyou@users.noreply.github.com>
2023-03-10 16:41:05 -0500

    Vendors Page

--- src/pages/dashboard/vendors/index.tsx
+++ src/pages/dashboard/vendors/index.tsx
@@ -1,17 +1,126 @@
 // pages/index.tsx
 
+import { Accordion, Dialog, Flex, Modal, Text, TextInput } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 import type { GetServerSideProps } from "next";
-import type { ReactElement } from "react";
+import Link from "next/link";
+import { ReactElement, useState } from "react";
 import { AppButton } from "../../../components/AppButton";
 import { Layout } from "../../../components/Layout";
-import { PartsTable } from "../../../components/PartsTable";
 import { getBasicServerSideProps } from "../../../services/getBasicSeverSideProps";
+import { api } from "../../../utils/api";
 import type { NextPageWithLayout } from "../../_app";
+import { useForm, zodResolver } from "@mantine/form";
+import { z } from "zod";
 
-const Manufacturers: NextPageWithLayout = () => {
+export const vendorSchema = z.object({
+  name: z.string({ required_error: "Required" }),
+});
+
+const Vendors: NextPageWithLayout = () => {
+  const allVendors = api.vendors.getAllVendors.useQuery();
+  const [vendorForDelete, setVendorForDelete] = useState("");
+  const [openedDialog, setOpenedDialog] = useState(false);
+  const [opened, { open, close }] = useDisclosure(false);
+
+  const { mutate: createVendor } = api.vendors.createVendor.useMutation({
+    onError: () => {
+      console.log("error");
+    },
+    onSuccess: async () => {
+      console.log("success");
+      await allManufacturers.refetch();
+      close();
+      // void queryClient.parts.getAllPartsFull.refetch();
+    },
+  });
+
+  const deleteManufacturer = api.manufacturers.deleteManufacturer.useMutation({
+    onSuccess: async () => {
+      await allManufacturers.refetch();
+    },
+  });
+
+  const form = useForm({
+    validate: zodResolver(manufacturerSchema),
+    initialValues: {
+      name: "",
+    },
+  });
+
+  console.log(allManufacturers);
   return (
     <>
-      <AppButton label="Placeholder for Detail View" />
+      <AppButton label="New Manufacturer" onClick={open} />
+      <Modal
+        opened={opened}
+        onClose={close}
+        title="Add New Manufacturer"
+        centered
+      >
+        <form onSubmit={form.onSubmit((values) => createManufacturer(values))}>
+          <TextInput
+            withAsterisk
+            label="Manufacturer Name"
+            mt="sm"
+            {...form.getInputProps("name")}
+          />
+          <div className="mt-2 flex items-center justify-around">
+            <AppButton label={"Submit"} type="submit" />
+            <AppButton label={"Clear"} onClick={() => form.reset()}></AppButton>
+          </div>
+        </form>
+      </Modal>
+      <Dialog position={{ left: "50%", top: "25%" }} opened={openedDialog}>
+        <Text>
+          Confirm manufacturer deletion?
+          <Flex>
+            <AppButton
+              label="Delete"
+              onClick={() => {
+                setOpenedDialog(false);
+                deleteManufacturer.mutate(manufacturerForDelete);
+              }}
+            />
+            <AppButton
+              label="Cancel"
+              onClick={() => {
+                setOpenedDialog(false);
+              }}
+            />
+          </Flex>
+        </Text>
+      </Dialog>
+      <Accordion defaultValue="customization">
+        {allManufacturers.data?.map((manufacturer) => {
+          return (
+            <Accordion.Item
+              value={manufacturer.name.toString()}
+              key={manufacturer.name}
+            >
+              <Accordion.Control>{manufacturer.name}</Accordion.Control>
+              <Accordion.Panel>
+                {manufacturer.manufacturerParts?.map((part) => {
+                  return (
+                    <div key={part.id} className="flex items-center gap-2">
+                      <Text className="w-1/5">{part.partNumber ?? ""}</Text>
+                      <Text className="w-auto">{part.description ?? ""}</Text>
+                    </div>
+                  );
+                })}
+                <AppButton
+                  label="Delete"
+                  hidden={manufacturer.manufacturerParts.length != 0}
+                  onClick={() => {
+                    setOpenedDialog(true);
+                    setManufacturerForDelete(manufacturer.name);
+                  }}
+                />
+              </Accordion.Panel>
+            </Accordion.Item>
+          );
+        })}
+      </Accordion>
     </>
   );
 };
---
+++ src/server/api/routers/vendors.ts
@@ -0,0 +1,72 @@
+import { z } from "zod";
+import { vendorSchema } from "../../../pages/dashboard/vendors";
+
+import { createTRPCRouter, publicProcedure } from "../trpc";
+
+export const vendorRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+
+  getAllVendors: publicProcedure.query(async ({ ctx }) => {
+    const vendors = await ctx.prisma.vendor.findMany({
+      select: {
+        name: true,
+      },
+    });
+    return vendors;
+  }),
+
+  deleteVendor: publicProcedure
+    .input(z.string())
+    .mutation(async ({ input, ctx }) => {
+      const vendor = await ctx.prisma.vendor.findFirst({
+        where: {
+          name: input,
+        },
+        include: {
+          VendorParts: true,
+        },
+      });
+
+      if (!vendor) {
+        throw new Error(`Vendor with name ${input} not found`);
+      }
+
+      const partCount = await ctx.prisma.vendorPart.count({
+        where: {
+          Vendor: {
+            name: input,
+          },
+        },
+      });
+
+      if (partCount > 0) {
+        throw new Error(`Cannot delete vendor with associated vendorParts`);
+      }
+
+      return await ctx.prisma.vendor.delete({
+        where: {
+          name: input,
+        },
+      });
+    }),
+
+  createVendpr: publicProcedure
+    .input(vendorSchema)
+    .mutation(async ({ input, ctx }) => {
+      return await ctx.prisma.vendor.create({
+        data: {
+          ...input,
+        },
+      });
+    }),
+
+  getSecretMessage: publicProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
