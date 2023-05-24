4f8a14df1535112f644ae37507f879bb952cc6fb
mjwillgetyou <44821370+mjwillgetyou@users.noreply.github.com>
2023-05-24 09:31:19 -0400

    eslint rule change

--- src/pages/dashboard/projects/[pid].tsx
+++ src/pages/dashboard/projects/[pid].tsx
@@ -23,10 +23,10 @@ const ProjectDetailView: NextPageWithLayout = () => {
     <>
       <div className="flex justify-between">
         <div className="g-x-1 w-2/5">
-          {/*eslint-disable-next-line @typescript-eslint/restrict-template-expressions*/}
-          <Text size="lg">{`Project: ${project.data?.projectNumber}`}</Text>{" "}
-          {/*eslint-disable-next-line @typescript-eslint/restrict-template-expressions*/}
-          <Text size="lg">{`Lead: ${project.data?.projectLead}`}</Text>
+          <Text size="lg">{`Project: ${
+            project.data?.projectNumber || ""
+          }`}</Text>{" "}
+          <Text size="lg">{`Lead: ${project.data?.projectLead || ""}`}</Text>
         </div>
         <div>
           <Text className="w-full" size="lg">
